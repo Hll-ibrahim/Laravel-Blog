45192aa57992786e0e8c877a93721d918894d085
Halil Ibrahim Ozkul <ibrahimozkul@gmail.com>
2022-06-24 03:40:37 +0300

    Article tablosu olusturuldu

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {/*
+        for($i=0; $i<4; $i++) {
+          DB::table('articles')->insert([
+            'category_id' = rand(1, 7),
+            'title' = 'Lorem',
+            'content' = 'Lorem ipsum dolor sit amet',
+            'slug' = str_slug(title),
+            'created_at' = now(),
+            'updated_at' = now()
+          ]);*/
+        }
+    }
+}
--- resources/views/front/homepage.blade.php
+++ resources/views/front/homepage.blade.php
@@ -1,4 +1,6 @@
 
+
+
 @extends('front.layouts.master')
 @section('title')
   Anasayfa
@@ -9,61 +11,26 @@
 
                 <div class="col-md-9 col-lg-8 col-xl-7">
                     <!-- Post preview-->
+                    @foreach($articles as $article)
                     <div class="post-preview">
                         <a href="post.html">
-                            <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
-                            <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                            <h2 class="post-title">{{$article->title}}</h2>
+                            <h3 class="post-subtitle">{{Str::limit($article->content, 50)}}</h3>
                         </a>
                         <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 24, 2022
+                            <a href="#" class="mr-4">{{$article->getCtegory->name}}</a>
+                            <span class="float-right">{{$article->created_at->diffForHumans()}}</span>
                         </p>
                     </div>
+                    @if(!$loop->last)
+                      <hr class="my-4" />
+                    @endif
+                    @endforeach
                     <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html"><h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2></a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 18, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Science has not yet mastered prophecy</h2>
-                            <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on August 24, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Failure is not an option</h2>
-                            <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on July 8, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Pager-->
-                    <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
-                </div>
 
-@include('front.widget.categoryWidget');
+
+                    <!-- Pager-->
+                  </div>
+
+@include('front.widget.categoryWidget')
 @endsection
