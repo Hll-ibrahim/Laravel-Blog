7dcd24b2f3204b66482a5211f44642f85c489e87
Halil Ibrahim Ozkul <ibrahimozkul@gmail.com>
2022-08-13 19:44:32 +0300

    Admin paneli giris sayfası tamamlandı

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Front\Homepage;
+use App\Http\Controllers\Back\Dashboard;
+use App\Http\Controllers\Back\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Backend Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+*/
+
+Route::get('admin/panel',[Dashboard::class, 'index'])->name('admin.dashboard');
+Route::get('admin/giris', [Auth::class, 'login'])->name('admin.login');
+
+
+/*
+|--------------------------------------------------------------------------
+| Frontend Routes
+|--------------------------------------------------------------------------
 */
 
 //Route::get('/', [Homepage::class, 'index']);
@@ -22,6 +30,4 @@ Route::get('category/{category}', [Homepage::class, 'category'])->name('category
 Route::get('/{category}/{slug}', [Homepage::class, 'single'])->name('single');
 Route::get('/{sayfa}',[Homepage::class, 'page'])->name('page');
 
-/*Route::get('/', function() {
-  return "salamlar";
-});*/
+
