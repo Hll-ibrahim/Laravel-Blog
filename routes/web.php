b79dd34a108ee0e75bbf5788ce5e0414db288611
Halil Ibrahim Ozkul <ibrahimozkul@gmail.com>
2022-08-14 15:36:35 +0300

    Middleware guvenlik islemleri tamamlandı

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,15 @@ use App\Http\Controllers\Back\AuthController;
 |--------------------------------------------------------------------------
 */
 
-Route::get('admin/panel',[Dashboard::class, 'index'])->name('admin.dashboard');
-Route::get('admin/giris', [AuthController::class, 'login'])->name('admin.login');
-Route::post('admin/giris', [AuthController::class, 'loginPost'])->name('admin.login.post');
-Route::get('admin/cikis', [AuthController::class, 'logout'])->name('admin.logout');
+Route::prefix('admin')->name('admin.')->middleware('isLogin')->group(function() {
+    Route::get('giris', [AuthController::class, 'login'])->name('login');
+    Route::post('giris', [AuthController::class, 'loginPost'])->name('login.post');
+});
+Route::prefix('admin')->name('admin.')->middleware('isAdmin')->group(function() {
+    Route::get('panel',[Dashboard::class, 'index'])->name('dashboard');
+    Route::get('cikis', [AuthController::class, 'logout'])->name('logout');
+    
+});
 
 
 /*
